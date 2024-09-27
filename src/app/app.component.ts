95a49a2e58bc56b047e162c0b019f87afc5c9798
IZanami77 <izanaminookami7@gmail.com>
2024-09-26 19:30:11 -0600

    Segunda clase

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./pages/home/home.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  VerPaisajesRel(){
+    alert("Ver mas paisajes relacionados")
+  }
+  VerOto(){
+    alert("Ver mas Otoños")
+  }
+}
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, input } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  @Input() image!: string;
+  @Input() title!: string;
+  @Input() info!: string;
+  @Input() text_btn!: string;
+  @Input() ClickEvento!:() => void
+
+  OnClick(){
+    this.ClickEvento();
+  }
+}
